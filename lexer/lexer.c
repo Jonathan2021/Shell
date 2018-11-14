9684559a7fbb0caca1df4e84d3b24c691a4eae94
Victor Chemla <victor.chemla@epita.fr>
2018-11-14 14:36:58 +0100

    More automate

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -46,4 +46,43 @@ int list(struct Token *t)
 int and_or(struct Token *t)
 {
 }
+int rule_if (struct Token *t)
+{
+    if (strcmp("if", t->name) != 0)
+        return 0;
+    if (list(t) == 0)
+        return 0;
+    if (strcmp("then", t->name) != 0)
+        return 0;
+    else_clause(t);
+    if (strcmp("fi", t->name) != 0)
+        return 0;
+}
 
+int else_clause(struct Token *t)
+{
+    if (strcmp(t->name, "else") == 0)
+    {
+        if (list(t) == 0)
+            return 0;
+        return 1;
+    }
+    else
+    {
+        if (strcmp(t->name, "elif") == 0)
+        {
+            if (list(t) == 0)
+                return 0;
+            if (strcmp("then",t->name) != 0)
+                return 0;
+            if (list(t) == 0)
+                return 0;
+            else_clause(t);
+            return 1;
+
+        }
+        else
+            return 0;
+    }
+
+}
