5b40f2346da21d84f47e87c910c29f5c9edcde69
Julie Siahou <julie.siahou@epita.fr>
2018-11-14 14:17:10 +0100

    main with token for lexer

---
+++ src/include/shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+# define SHELL_H
+
+struct token
+{
+    char *name;
+    char *type;
+    struct token *next;
+};
+
+void add_token(struct token **token, char *str);
+struct token *parse_path(char * str);
+
+#endif /* !SHELL_H */
---
+++ src/main.c
@@ -0,0 +1,85 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <err.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include "include/shell.h"
+
+void add_token(struct token **token, char *str)
+{
+    char *grammar[20][20] = {
+    {"WORD","ls","pwd","cd","\0"},
+    {"SEMICOLON",";","\0"},
+    {"OP","<<",">>","<&",">&","<>","<<-","\0"},
+    {"CLOBBER",">|","\0"},
+    {"IF","if","\0"},
+    {"FI","fi","\0"},
+    {"THEN","then","\0"},
+    {"ELIF","elif","\0"},
+    {"ELSE","else","\0"},
+    {"LOOP","case","esac","while","until","for","\0"},
+    {"BRACE","{","}","!","\0"},
+    {"IN","in","\0"}
+    };
+    struct token *next = malloc(sizeof(struct token));
+    for(int i = 0; i < 12; i++)
+    {
+        for (int j = 0; grammar[i][j][0] != '\0'; j++ )
+        {
+            if (strcmp(grammar[i][j],str) == 0)
+            {
+                next->name = str;
+                next->type = grammar[i][0];
+                next->next = NULL;
+            }
+        }
+    }
+    if (!*token)
+    {
+        *token = next;
+        return;
+    }
+    struct token *copy = *token;
+    while (copy->next)
+    {
+        copy = copy->next;
+    }
+    copy->next = next;
+}
+
+struct token *parse_path(char * str)
+{
+    char *parse;
+    char *delim = {"\n\t "};
+    parse = strtok(str,delim);
+    struct token *token = NULL;
+    while (parse)
+    {
+        add_token(&token,parse);
+        parse = strtok(NULL,delim);
+    }
+    return token;
+}
+
+int main(void)
+{
+    //if (isatty(0))
+    char str[4095];
+    printf("42sh$ ");
+    while(fgets(str,4095,stdin))
+    {
+        struct token *token = parse_path(str);
+        while(token)
+        {
+            printf("->%s",token->type);
+            token = token->next;
+        }
+        printf("\n");
+        printf("42sh$ ");
+    }
+    return 0;
+} 
