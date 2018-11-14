d6370733d3dd2f3347317eaa346d13121f9c508c
Victor Chemla <victor.chemla@epita.fr>
2018-11-14 21:02:26 +0100

    [curving] remove the \n if nothibg is written

--- src/main.c
+++ src/main.c
@@ -9,22 +9,23 @@
 #include <sys/stat.h>
 #include "include/shell.h"
 
+
 void add_token(struct Token **token, char *str)
 {
     char *grammar[20][20] =
     {{"WORD","ls","pwd","cd","\0"},
-    {"SEMICOLON",";","\0"},
-    {"OP_LOGIQUE","&&","||",";;","\0"},
-    {"OP_IO","<<",">>","<&",">&","<>","<<-","\0"},
-    {"CLOBBER",">|","\0"},
-    {"IF","if","\0"},
-    {"FI","fi","\0"},
-    {"THEN","then","\0"},
-    {"ELIF","elif","\0"},
-    {"ELSE","else","\0"},
-    {"LOOP","case","esac","while","until","for","\0"},
-    {"BRACE","{","}","!","\0"},
-    {"IN","in","\0"}};
+        {"SEMICOLON",";","\0"},
+        {"OP_LOGIQUE","&&","||",";;","\0"},
+        {"OP_IO","<<",">>","<&",">&","<>","<<-","\0"},
+        {"CLOBBER",">|","\0"},
+        {"IF","if","\0"},
+        {"FI","fi","\0"},
+        {"THEN","then","\0"},
+        {"ELIF","elif","\0"},
+        {"ELSE","else","\0"},
+        {"LOOP","case","esac","while","until","for","\0"},
+        {"BRACE","{","}","!","\0"},
+        {"IN","in","\0"}};
     struct Token *next = malloc(sizeof(struct Token));
     for(int i = 0; i < 12; i++)
     {
@@ -64,25 +65,29 @@ struct Token *parse_path(struct Token *token,char * str)
     return token;
 }
 
-int main(void)
+struct Token*carving(void)
 {
-    //if (isatty(0))
     char str[4095];
+    int ret = 0;
     if (isatty(0))
         printf("42sh$ ");
     struct Token *token = NULL;
     while(fgets(str,4095,stdin))
     {
+        ret = 0;
         if (strncmp(str,"exit",4) == 0)
             exit(0);
         token = parse_path(token,str);
         while(token)
         {
+            ret = 1;
             printf("->%s",token->type);
             token = token->next;
         }
+        if (ret == 1)
+            printf("\n");
         if (isatty(0))
-            printf("\n42sh$ ");
+            printf("42sh$ ");
     }
     if (!isatty(0))
     {
@@ -93,5 +98,18 @@ int main(void)
         }
     }
     printf("\n");
+    return 0;
+}
+void print_t(struct Token *t)
+{
+    for (;t != NULL ; t = t->next)
+    {
+        printf("%s ",t->name);
+    }
+}
+
+int main(void)
+{
+    carving();
     return 0;
 } 
