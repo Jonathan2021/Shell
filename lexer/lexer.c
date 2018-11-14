bbdc8767fc157b96d36ab5261ee4673766f498de
Thomas gossin <Thomas.gossin@epita.fr>
2018-11-14 16:24:09 +0100

    [lexer] merge lexer part

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -53,10 +53,10 @@ int and_or(struct Token **t)
         return 0;
     while(1)
     {
-         if (check == 1)
-             *t = cpy;
-         if (check == 0)
-             return 1;
+        if (check == 1)
+            *t = cpy;
+        if (check == 0)
+            return 1;
         if (strcmp(";", cpy->name) == 0 || strcmp("&", cpy->name) == 0)
         {
             cpy = cpy->next;
@@ -82,77 +82,68 @@ int and_or(struct Token **t)
 }
 int rule_if (struct Token **t)
 {
-    struct Token **tmp = t;
-    if (strcmp("if", t->name) != 0)
+    struct Token *tmp = *t;
+    if (strcmp("if", tmp->name) != 0)
     {
-        t = tmp;
         return 0;
     }
-    *t = *t->next;
-    if (list(t) == 0)
+    tmp = tmp->next;
+    if (list(&tmp) == 0)
     {
-        t = tmp;
         return 0;
     }
-    *t = *t->next;
-    if (strcmp("then", t->name) != 0)
+    if (strcmp("then", tmp->name) != 0)
     {
-        t = tmp;
         return 0;
     }
-    else_clause(t);
-    *t = *t->next;
-    if (strcmp("fi", t->name) != 0)
+    tmp = tmp->next;
+    if (list(&tmp) == 0)
+        return 0;
+    else_clause(&tmp);
+    if (strcmp("fi", tmp->name) != 0)
     {
-        t = tmp;
         return 0;
     }
+    tmp = tmp->next;
+    *t = tmp;
     return 1;
 }
 
 int else_clause(struct Token **t)
 {
-    struct Token **tmp = t;
-    if (strcmp(t->name, "else") == 0)
+    struct Token *tmp = *t;
+    if (strcmp(tmp->name, "else") == 0)
     {
-        if (list(t) == 0)
+        tmp = tmp->next;
+        if (list(&tmp) == 1)
         {
-            t = tmp;
-        }
-        else
-        {
-            *t = *t->next;
+            t* = tmp;
             return 1;
         }
+
+    }
+    tmp = *t;
+    if (strcmp(tmp->name, "elif") == 0)
+    {
+        tmp = tmp->next;
+        if (list(&tmp) == 0)
+            return 0;
+
+        if (strcmp("then",tmp->name) != 0)
+            return 0;
+        *t = *t->next;
+
+        if (list(&tmp) == 0)
+            return 0;
+
+        else_clause(&tmp);
+        *t = tmp;
+        return 1;
+
     }
     else
-    {
-        t = tmp;
-        if (strcmp(t->name, "elif") == 0)
-        {
-            if (list(t) == 0)
-                return 0;
-            *t = *t->next;
-            if (strcmp("then",t->name) != 0)
-            {
-                t = tmp;
-                return 0;
-            }
-            *t = *t->next;
-            if (list(t) == 0)
-            {
-                t = tmp;
-                return 0;
-            }
-            *t = *t->next;
-            else_clause(t);
-            return 1;
-
-        }
-        else
-            return 0;
-    }
-}   
+        return 0;
+}
 int shell_command(struct Token **t)
 {
     struct Token *t1 = *t;
