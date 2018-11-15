16bf5ee97feee18fa2959a73d29359af0a690c39
Jonathan Sands <misterjonathansands@gmail.com>
2018-11-15 17:23:56 +0100

    AST struct and basic funcs

---
+++ src/parser/if_node.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "my_tree.h"
+
+struct AST *if_init()
+{
+    struct AST *node = AST_init(3);
+    node->name = "if";
+    //node->child[0] = condition
+    //node->child[1] = ifbody
+    //node->child[2] = else body
+    return node;
+}
+
+int foo_if(struct AST *node)
+{
+    if (node->child[0]->res)
+    {
+        //go dans fils 1;
+        node->res = 1;
+        return 1;
+    }
+    //go dans fils 2;
+    node->res = 0;
+    return 0;
+}
---
+++ src/parser/my_tree.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "my_tree.h"
+
+struct AST *AST_init(int nb_child)
+{
+    struct AST *new = malloc(sizeof(struct AST));
+    if (new == NULL)
+        return NULL;
+    new->child = malloc(sizeof(struct AST) * nb_child);
+    new->nb_child = nb_child;
+    for (int i = 0; i < nb_child; i++)
+    {
+        new->child[i] == NULL;
+    }
+    new->res = 0;
+    return new;
+}
+void AST_print_prefix(const struct AST *tree)
+{
+    if (tree != NULL)
+    {
+        printf(tree->name);
+        for (int i = 0; i < tree->nb_child; i++)
+        {
+            AST_print_infix(tree->child[i]);
+        }
+    }
+}
+
+void AST_destroy(struct AST *s)
+{
+    if (s == NULL)
+        return;
+
+    for (int i = 0; i < s->nb_child; i++)
+    {
+        AST_destroy(s->child[i]);
+    }
+    free(s->child);
+    free(s);
+}
