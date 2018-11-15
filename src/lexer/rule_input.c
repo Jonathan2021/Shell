4cd2fc482a5b5ab5901e34ba984b67e1b5ecb266
Thomas gossin <thomas.gossin@epita.fr>
2018-11-15 22:13:25 +0100

    [parser] AST init

---
+++ src/lexer/rule_input.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "include/my_tree.h"
+
+
+struct AST *input(struct Token **t)
+{
+    struct AST *node;
+    struct Token *tmp = *t;
+    if ((node = list(&tmp)) == 1)
+    {
+        if (tmp == NULL)
+        {
+            *t = tmp;
+            return node;
+        }
+        else
+            return NULL;
+    }
+    return NULL;
+}
