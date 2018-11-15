b438796f6798412193877df850a393afb951ddd1
Thomas gossin <thomas.gossin@epita.fr>
2018-11-15 22:21:56 +0100

    [parser] test 1

--- src/lexer/rule_list.c
+++ src/lexer/rule_list.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "include/my_tree.h"
+#include "include/rule.h"
 
 struct AST *list_init(void)
 {
--- src/lexer/word_node.c
+++ src/lexer/word_node.c
@@ -1,6 +1,8 @@
-nclude <stdio.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include "include/my_tree.h"
+#include "include/rule.h"
+
 
 
 
