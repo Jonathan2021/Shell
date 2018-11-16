56caaebe1eb513ac1c764be0552df47f808413bf
Jonathan Sands <misterjonathansands@gmail.com>
2018-11-16 16:05:21 +0100

    rule_input, fixed rle_input + made foo_input

--- src/lexer/rule_input.c
+++ src/lexer/rule_input.c
@@ -4,22 +4,42 @@
 #include "include/rule.h"
 
 
+struct AST *input_init()
+{
+    struct Token *token = malloc(sizeof(struct Token));
+    if(!token)
+        return NULL;
+    struct AST *node = AST_init(1);
+    if(!node)
+    {
+        free(token);
+        return NULL;
+    }
+    token->type = "INPUT";
+    token->name = "input";
+    node->self = token;
+    return node;
+}
 struct AST *input(struct Token **t)
 {
-    struct AST *node;
+    struct AST *node = input_init();
     struct Token *tmp = *t;
-    if ((node = list(&tmp)) != NULL)
-    {
-        if (tmp == NULL)
-        {
-            *t = tmp;
-            return node;
-        }
-        else
-        {
-            AST_destroy(node);
-            return NULL;
-        }
-    }
+    node->child[0] = list(&tmp);
+    if(!tmp || !strcmp(tmp->name, "\n")
+        return node;
+    AST_destroy(node);
     return NULL;
 }
+
+int foo_input(struct AST *node)
+{
+    if(!node)
+        return 0;
+    if(node->child[0])
+        node->res = node->child[0]->res;
+    else
+        node->res = 1;
+    return node->res;
+}
+
+
