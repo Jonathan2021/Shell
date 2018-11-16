c9d3f430cc2f352fb6f97c9c280ef7148c4c6a72
Victor Chemla <victor.chemla@epita.fr>
2018-11-16 05:08:37 +0100

    autogen good

---
+++ autogen.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+if [ "$1" != "clean" ] ; then
+    cp build/configure.ac .
+    cp build/Makefile.am .
+    autoreconf -vi
+else
+    make distclean
+    if [ -f "configure.ac" ] ; then
+        rm configure.ac
+    fi
+    if [ -f "Makefile.am" ] ; then
+        rm Makefile.am
+    fi
+    if [ -f "output.gv" ] ; then
+        rm output.gv
+    fi
+    rm -rf autom4te.cache/ compile config.h.in Makefile missing \
+    aclocal.m4 install-sh depcomp Makefile.in
+fi
