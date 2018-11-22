65ab0c836c652c4fa0f7c92f5007328961ebe7ca
Jonathan Sands <misterjonathansands@gmail.com>
2018-11-22 18:03:22 +0100

    fixed some not up-to-date foo + modified rule_shell_command

--- src/lexer/rule_shell_command.c
+++ src/lexer/rule_shell_command.c
@@ -17,6 +17,29 @@ struct AST *word_init(struct Token *token)
     return node;
 }
 
+void foo_shell_cmd(__attribute__((unused)) struct AST *node, __attribute__((unused))struct fds fd)
+{
+    return;
+}
+
+struct AST *shell_cmd_init(void)
+{
+    struct Token *token = malloc(sizeof(struct Token));
+    if(!token)
+        return NULL;
+    struct AST *node = AST_init(3);
+    if (!node)
+    {
+        free(token);
+        return NULL;
+    }
+    token->type = "SHELL COMMAND";
+    token->name = "SHELL COMMAND";
+    node->self = token;
+    node->foo = foo_shell_cmd;
+    return node;
+
+}
 //struct AST *assignment_word_init(struct Token *token)
 //{
 
@@ -26,54 +49,60 @@ struct AST *shell_command(struct Token **t)
 {
     struct Token *t1 = *t;
     struct Token *t2 = *t;
+    struct Token *ouvre;
+    struct Token *ferme;
     struct AST *shell = NULL;
     if (strcmp("{", t1->name) == 0 || strcmp("(", t1->name) == 0)
     {
+        ouvre = t1;
         t1 = t1->next;
         if (t1 == NULL)
             return NULL;
-        if ((shell = list(&t1)) != NULL)
+        if ((shell = compound_list(&t1)) != NULL)
         {
             if (t1 == NULL)
             {
+                //get_token();
                 AST_destroy(shell);
-                return NULL;
             }
-            if (strcmp("}", t1->name) == 0 || strcmp(")", t1->name) == 0)
+            if ((!strcmp("}", t1->name) || !strcmp(")", t1->name)) 
+                    && !strcmp(ouvre->name, t1->name))
             {
-                if (t1 != NULL)
-                    t1 = t1->next;
-                t = &t1;
-                return shell;
+                ferme = t1;
+                t1 = t1->next;
+                *t = t1;
+                struct AST *shell_node = shell_cmd_init();
+                shell_node->child[0] = word_init(ouvre);
+                shell_node->child[1] = shell;
+                shell_node->child[2] = word_init(ferme);
+                return shell_node;
             }
             else
                 AST_destroy(shell);
         }
     }
+    //C est degeu, deja j ai mis le return shell à la fin plutot que dans chaque else if, et en plus on peut factoriser cette mette en faisant des || et un seul else if et un seul *t = t2 mais flme alors que ca aurait été plus rapide que d ecrire ce message.
     else if ((shell = rule_for(&t2)) != NULL)
     {
         *t = t2;
-        return shell;
     }
     else if ((shell = rule_while(&t2)) != NULL)
     {
         *t = t2;
-        return shell;
     }
     else if ((shell = rule_until(&t2)) != NULL)
     {
         *t = t2;
-        return shell;
     }
     else if ((shell = rule_case(&t2)) != NULL)
     {
         *t = t2;
-        return shell;
     }
     else if ((shell = rule_if(&t2)) != NULL)
     {
         *t = t2;
-        return shell;
     }
-    return NULL;
+    else
+        return NULL;
+    return shell;
 }
