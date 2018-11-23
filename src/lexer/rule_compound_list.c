afc2b102ba6a468fae3222a65c1155473aa80a94
Jonathan Sands <misterjonathansands@gmail.com>
2018-11-23 19:10:59 +0100

    foo for

--- src/lexer/rule_compound_list.c
+++ src/lexer/rule_compound_list.c
@@ -61,6 +61,8 @@ int exec_init(struct AST *node, int *index, struct fds fd)
         cur_type = node->child[*index]->self->type;
         if (!strcmp(cur_name, ";") || !strcmp(cur_name, "&") 
             || !strcmp(cur_name, "\n"))
+        {
+            my_cmd[i] = NULL;
             break;
         if (strcmp(cur_type, "WORD"))
         {
