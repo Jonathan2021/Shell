c9792f816bc7c5961294360b8901b7bfc4e883f6
Jonathan Sands <misterjonathansands@gmail.com>
2018-11-26 17:28:01 +0100

    foo.h updated ps to ps2

--- src/lexer/include/foo.h
+++ src/lexer/include/foo.h
@@ -5,5 +5,5 @@
 
 void foo_compound(struct AST *node, struct fds fd);
 struct Token *get_Token(struct Token **tmp, struct Token **t);
-void call_ps(struct Token **t, struct Token **t2);
+void call_ps2(struct Token **t, struct Token **t2);
 #endif /* !_FOO_H_*/
