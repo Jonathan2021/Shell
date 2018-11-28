fa96ce99e0ac2804ba19310013ed8e4d7c78463a
Julie Siahou <julie.siahou@epita.fr>
2018-11-28 21:33:52 +0100

    add --norc

--- src/file/tool_file.c
+++ src/file/tool_file.c
@@ -9,9 +9,9 @@
 #include <sys/stat.h>
 #include "../include/shell.h"
 
-char *get_value(char *name)
+char *get_file(char *name)
 {
-    FILE *file = fopen("src/file/variable.txt","r+");
+    FILE *file = fopen("src/etc/42shrc","r+");
     if (!file)
         return NULL;
     char *check = malloc(60);
@@ -36,12 +36,12 @@ char *get_value(char *name)
     return NULL;
 }
 
-void set_value(char *name, char *value)
+void set_file(char *name, char *value)
 {
-    FILE *file = fopen("src/file/variable.txt","r");
+    FILE *file = fopen("src/etc/42shrc","r");
     if (!file)
         return;
-    FILE *copy = fopen("src/file/copy.txt","w+");
+    FILE *copy = fopen("src/etc/copy.txt","w+");
     char *check = malloc(60);
     char *data = malloc(60);
     int nb;
@@ -56,104 +56,16 @@ void set_value(char *name, char *value)
             fprintf(copy,"%s %s\n",check,data);
         }
     }
-    remove("src/file/variable.txt");
+    remove("src/etc/42shrc");
     free(check);
     fclose(file);
     fclose(copy);
-    rename("src/file/copy.txt","src/file/variable.txt");
+    rename("src/etc/copy.txt","src/etc/42shrc");
 
 }
 
-long str_to_argv(char **argv, char *str)
+void reset_file(void)
 {
-    char *parse;
-    char *delim = {"\n \t"};
-    parse = strtok(str,delim);
-    long i = 1;
-    char * cpy = malloc(4095);
-    while (parse)
-    {
-        if (strlen(parse) > 1 && ((parse[0] == '\"' && parse[strlen(parse)-1] == '\"' ) ||
-            (parse[0] == '\'' && parse[strlen(parse)-1] == '\'')))
-        {
-            argv[i] = parse+1;
-            argv[i][strlen(argv[i])-1] = 0;
-            i++;
-        }
-        else if (parse[0] == '\"' || strcmp(parse,"\"") == 0 ||
-            parse[0] == '\'' || strcmp(parse,"\'") == 0)
-        {
-            if (parse[0] == '\"' || parse[0] == '\'')
-                argv[i] = parse+1;
-            parse = strtok(NULL,delim);
-            while (parse && (parse[strlen(parse)-1] != '\"' && strcmp(parse,"\"") != 0 &&
-                parse[strlen(parse)-1] != '\'' && strcmp(parse,"\'") != 0))
-            {
-                strcpy(cpy,parse);
-                strcat(argv[i]," ");
-                strcat(argv[i],cpy);
-                parse = strtok(NULL,delim);
-            }
-            if (parse && strlen(parse) > 1 && (parse[strlen(parse)-1] == '\"' || parse[strlen(parse)-1] == '\''))
-            {
-                parse[strlen(parse)-1] = 0;
-                strcpy(cpy,parse);
-                strcat(argv[i]," ");
-                strcat(argv[i],cpy);
-            }
-            i++;
-        }
-        argv[i] = parse;
-        parse = strtok(NULL,delim);
-        i++;
-    }
-    free(cpy);
-    argv[i] = NULL;
-    return i;
-}
-
-void reset_value(void)
-{
-    set_value("IFS","\\t \\n");
-    set_value("--ast-print","0");
-    set_value("--type-print","0");
-    set_value("version","0");
-}
-
-
-void read_isatty(void)
-{
-    char str[4095];
-    int ret = 0;
-    struct Token *token = NULL;
-    while(fgets(str,4095,stdin))
-    {
-        token = NULL;
-        if (strncmp(str,"exit",4) == 0)
-        {
-            exit(0);
-        }
-        //argc = str_to_argv(argv,str);
-        //token = parse_path(token,argv,argc);
-        char *parse;
-        char *delim = {"\t \n"};
-        parse = strtok(str,delim);
-        while (parse)
-        {
-            add_token(&token,parse);
-            parse = strtok(NULL,delim);
-        }
-        struct Token *tmp = token;
-        while (tmp)
-        {
-            printf("->%s",tmp->type);
-            tmp = tmp->next;
-        }
-    }
-    if (ret == 1)
-    {
-        lexer(token);
-        printf("\n");
-    }
-    check_option(token);
+    set_file("PS1","");
+    set_file("PS2","");
 }
