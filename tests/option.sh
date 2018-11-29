a519d01eb8f846789176df387a395c5ec6812fa5
Julie Siahou <julie.siahou@epita.fr>
2018-11-29 06:10:28 +0100

    option on testsuit set without timeout

--- tests/option.sh
+++ tests/option.sh
@@ -12,6 +12,9 @@ for arg in "$@"; do
 done
 
 OPTIND=1
+sanity=0
+timeout=0
+category=""
 while getopts "lstc:" opt
 do
   case "$opt" in
@@ -20,9 +23,26 @@ do
           for folder in *; do
             echo $folder
           done;;
-    "c") echo "category $OPTARG";;
-    "s") echo "sanity";;
-    "t") echo "timeout";;
+    "c") category+="$OPTARG "; echo "category $OPTARG";;
+    "s") sanity=1; echo "sanity";;
+    "t") timeout=1; echo "timeout";;
   esac
 done
+
+if [ $category = "" ]; then
+  if [ $sanity -eq 1 ]; then
+      valgrind ./loadtest.sh
+  else
+      ./loadtest.sh
+  fi
+else
+  for test in $category; do
+    if [ $sanity -eq 1 ]; then
+      valgrind ./loadtest.sh $test
+    else
+      ./loadtest.sh $test
+    fi 
+  done
+fi
+
 shift $(expr $OPTIND - 1)
