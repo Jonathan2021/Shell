eee2b6c8640cd9995735cff4b41d394a2ad50792
Jonathan Sands <misterjonathansands@gmail.com>
2018-12-04 11:55:47 +0100

    added ASSIGMENT_WORD in ast-print and did foo_assigment that assigns a value

--- src/parser/prefix.c
+++ src/parser/prefix.c
@@ -10,33 +10,53 @@
 #include "include/my_tree.h"
 #include "include/rule.h"
 
-/**
- ** \brief init prefix node
- ** \param token linked list
- ** \return node prefix
- **/
-
-struct AST *prefix_init(struct Token *token)
+void foo_assigment(struct AST *node, __attribute__((unused))struct fds fd)
 {
-    struct AST *node = AST_init(1);
-    if (!node)
-        return NULL;
-    node->self = token;
-    return node;
+    if(!node)
+        return;
+    int i = 0;
+    for(; node->self->name[i] != '='; ++i);
+    node->self->name[i] = 0;
+    setvalue(node->self->name, getvalue(node->self->name + i + 1));
 }
 
+/**
+ ** \brief init assigment node
+ ** \param token linked list
+ ** \return node assigment
+ **/
+
+struct AST *assigment_init(struct Token *token)
+{
+    struct AST *node = AST_init(0);
+    if(!node)
+        return NULL;
+    token->type = "ASSIGMENT_WORD";
+    node->self = token;
+    node->foo = foo_assigment;
+    return node;
+}
 /**
  ** \brief check grammar prefix and create prefix node
  ** \param token linked list
  ** \return node prefix
  **/
 
+int is_assigment(char *str)
+{
+    if (!str || str[0] == '=' || (str[0] > '0' && str[0] < '9'))
+        return 0;
+    int i = 0;
+    for (; str[i] != '=' && str[i] != 0; ++i);
+    return (str[i] == '=');
+}
+
 struct AST *prefix(struct Token **t)
 {
     struct AST *redir;
-    if (strcmp(t[0]->type, "ASSIGMENT_WORD") == 0)
+    if (is_assigment(t[0]->name))
     {
-        struct AST *assigment_word = prefix_init(*t);
+        struct AST *assigment_word = assigment_init(*t);
         *t = t[0]->next;
         return assigment_word;
     }
