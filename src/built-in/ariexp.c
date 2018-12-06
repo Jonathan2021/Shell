bf73c4a188486cdef09764b2d2cfb810bb9bbbc3
Victor Chemla <victor.chemla@epita.fr>
2018-12-06 06:53:00 +0100

    begin of arithmetic expension

---
+++ src/built-in/ariexp.c
@@ -0,0 +1,48 @@
+#define _XOPEN_SOURCE 600
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+char **split(char *str, char delim)
+{
+    char **rslt = malloc(sizeof(char *) * strlen(str));
+    int i = 0;
+    int gi = 0;
+    for (; str[i] != '\0'; i++)
+    {
+        char *tmp = malloc(strlen(str));
+        int j = 0;
+        for (; str[i] != delim && str[i] != '\0'; j++, i++)
+        {
+            if (str[i] != ' ')
+                tmp[j] = str[i];
+            else
+                j--;
+        }
+        tmp[j] = '\0';
+        rslt[gi++] = tmp;
+        if (str[i] == '\0')
+            break;
+    }
+    rslt[gi] = '\0';
+    return rslt;
+}
+
+int expen(char *str)
+{
+    char **cut = split(str, '+');
+    if (cut[1] == '\0')
+        printf("NOT GOOD\n");
+    else
+        printf("%s add %s", getvalue(cut[0]), cut[1]);
+    for (int i = 0; cut[i] != '\0' ; i++)
+        free(cut[i]);
+    free(cut);
+    return 0;
+}
+int main(int argc, char *argv[])
+{
+  expen("i    + w");
+  return 0;
+}
