411c5879f6e52a1bd6881ecd7c15587add9c16b7
Julie Siahou <julie.siahou@epita.fr>
2018-12-06 19:25:40 +0100

    add folder include for builtin

---
+++ src/built-in/include/built-in.h
@@ -0,0 +1,10 @@
+#ifndef H_BUILTIN
+#define H_BUILTIN
+
+#include "../..//parser/include/my_tree.h"
+int my_cd(char **str);
+int my_echo(char **args, struct fds fd);
+char *remove_next(char *txt, int i);
+int my_exit(struct AST *ast, struct Token *token);
+int my_shopt(char **str);
+#endif /* ! H_BUILTIN */
