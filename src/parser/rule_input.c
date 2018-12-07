7ed0bee20d46fdcc0d3635f38f2137bf79f96264
Thomas gossin <Thomas.gossin@epita.fr>
2018-12-07 16:03:09 +0100

    fix in rule list for backspace

--- src/parser/rule_input.c
+++ src/parser/rule_input.c
@@ -19,11 +19,11 @@ struct AST *input(struct Token **t)
 {
     struct AST *node;
     struct Token *tmp = *t;
-    if (tmp == NULL || strcmp(tmp->name, "\n") == 0)
+    if (tmp == NULL || ((strcmp(tmp->name, "\n") == 0) && tmp->next == NULL))
         return NULL;
     if ((node = list(&tmp)) != NULL)
     {
-        if (tmp == NULL || strcmp(tmp->name, "\n") == 0)
+        if (tmp == NULL || ((strcmp(tmp->name, "\n") == 0)))
         {
             *t = tmp;
             return node;
